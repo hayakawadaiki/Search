4f9fe4ffe22e8697c7f837f79a5ffb6950a34152
hayakawadaiki <tumura14@yahoo.co.jp>
2021-07-09 19:30:03 +0900

    修正

--- database/migrations/2021_07_01_000000_create_user_person_charges_table.php
+++ database/migrations/2021_07_01_000000_create_user_person_charges_table.php
@@ -11,8 +11,8 @@ class CreateUserPersonChargesTable extends Migration
         Schema::create('user_person_charges', function (Blueprint $table) {
             $table->integer('id')->autoIncrement()->comment('id');
             $table->integer('user_id')->comment('ユーザーid');
-            $table->integer('math_teacher_user_id')->nullable()->comment('数学講師id');
             $table->integer('japanese_language_user_id')->nullable()->comment('国語講師id');
+            $table->integer('math_teacher_user_id')->nullable()->comment('数学講師id');
             $table->timestamp('created_at')->useCurrent()->comment('登録日時');
             $table->timestamp('updated_at')->default(DB::raw('current_timestamp on update current_timestamp'))->comment('更新日時');
             $table->softDeletes()->comment('削除日時');
--- database/migrations/2021_07_01_000000_create_users_table.php
+++ database/migrations/2021_07_01_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('email', 255)->unique()->comment('メールアドレス');
             $table->string('password', 255)->comment('パスワード');
             $table->integer('role')->default(10)->nullable()->comment('権限');
-            $table->rememberToken();
             $table->timestamp('created_at')->useCurrent()->comment('登録日時');
             $table->timestamp('updated_at')->default(DB::raw('current_timestamp on update current_timestamp'))->comment('更新日時');
             $table->softDeletes()->comment('削除日時');
